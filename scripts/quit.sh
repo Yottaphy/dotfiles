48e62c5a7d85544cdc9b9645027201b01d9eb506
Yottaphy <jorge13romero@gmail.com>
2022-05-28 12:55:58 +0300

    added and fixed quit.sh script

---
+++ scripts/quit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#echo -e "cancel\nshutdown\nreboot" | dmenu -fn 'UbuntuMono Nerd Font:style=Bold' -h 20 -l 3 -nb "#1d1f21" -nf "#fba922" -sb "#fba922" -sf "#1d1f2" 
+
+selection=$(echo -e "cancel\nlogout\nreboot\nshutdown" | rofi -dmenu -theme "/home/yottaphy/.config/rofi/themes/yquit.rasi") 
+
+#-fn 'UbuntuMono Nerd Font:style=Bold' -h 20 -l 3 | xargs )
+
+if [[ $selection = "logout" ]]; then
+   bspc quit
+elif [[ $selection = "shutdown" ]]; then
+    systemctl poweroff
+elif [[ $selection = "reboot" ]]; then
+    systemctl reboot
+else exit
+fi
+
