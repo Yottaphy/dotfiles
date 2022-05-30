17cf732fcff8460664610f50292d113193dda488
Yottaphy <jorge13romero@gmail.com>
2022-05-30 09:58:40 +0200

    updated quit.sh with icons

--- scripts/quit.sh
+++ scripts/quit.sh
@@ -2,15 +2,15 @@
 
 #echo -e "cancel\nshutdown\nreboot" | dmenu -fn 'UbuntuMono Nerd Font:style=Bold' -h 20 -l 3 -nb "#1d1f21" -nf "#fba922" -sb "#fba922" -sf "#1d1f2" 
 
-selection=$(echo -e "cancel\nlogout\nreboot\nshutdown" | rofi -dmenu -theme "/home/yottaphy/.config/rofi/themes/yquit.rasi") 
+selection=$(echo -e " \tcancel\n \tlogout\n 累\treboot\n 襤\tpower" | rofi -dmenu -theme "/home/yottaphy/.config/rofi/themes/yquit.rasi") 
 
 #-fn 'UbuntuMono Nerd Font:style=Bold' -h 20 -l 3 | xargs )
 
-if [[ $selection = "logout" ]]; then
+if [[ $selection = " \tlogout" ]]; then
    bspc quit
-elif [[ $selection = "shutdown" ]]; then
+elif [[ $selection = " 襤\tpower" ]]; then
    shutdown now
-elif [[ $selection = "reboot" ]]; then
+elif [[ $selection = " 累\treboot" ]]; then
    reboot
 else exit
 fi
