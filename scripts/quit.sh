1121f5f99e3063b3682716ff3a80addf2f50e02f
Yottaphy <jorge13romero@gmail.com>
2022-06-01 00:42:36 +0200

    changed rofi themes

--- scripts/quit.sh
+++ scripts/quit.sh
@@ -2,15 +2,16 @@
 
 #echo -e "cancel\nshutdown\nreboot" | dmenu -fn 'UbuntuMono Nerd Font:style=Bold' -h 20 -l 3 -nb "#1d1f21" -nf "#fba922" -sb "#fba922" -sf "#1d1f2" 
 
-selection=$(echo -e " \tcancel\n \tlogout\n 累\treboot\n 襤\tpower" | rofi -dmenu -theme "/home/yottaphy/.config/rofi/themes/yquit.rasi") 
+selection=$(echo -e " \tcancel\n \tlogout\n 累\treboot\n 襤\tpower" | rofi -dmenu -theme "/home/yottaphy/.config/rofi/themes/yquit.rasi")
 
+echo $selection
 #-fn 'UbuntuMono Nerd Font:style=Bold' -h 20 -l 3 | xargs )
 
-if [[ $selection = " \tlogout" ]]; then
+if [[ $selection = *"logout"* ]]; then
    bspc quit
-elif [[ $selection = " 襤\tpower" ]]; then
+elif [[ $selection = *"power"* ]]; then
    shutdown now
-elif [[ $selection = " 累\treboot" ]]; then
+elif [[ $selection = *"reboot"* ]]; then
    reboot
 else exit
 fi
