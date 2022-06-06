11517275a3c7c99bfa42f2f97fbb94fa27e5b7ad
Yottaphy <jorge13romero@gmail.com>
2022-06-06 12:15:44 +0200

    changed battery script

--- scripts/batterynotification.sh
+++ scripts/batterynotification.sh
@@ -3,14 +3,14 @@ while sleep 5; do
 	read -r status capacity < <(acpi -b | awk -F'[,:%]' '{print $2, $3}')
 	if [ "$status" = Discharging ] && [ "$capacity" -lt 5 ] && [ "$critbatnotsent" != yes ]; then
 		critbatnotsent=yes
-		mpv --no-config /usr/local/share/ding.mp3; notify-send 'Battery level critically low!' 'Suspending in 60 seconds.' &
+		mpv --no-config /usr/local/share/ding.mp3; notify-send --app-name='Battery level critically low!' 'Suspending in 60 seconds.' &
 	    sleep 60; read -r status capacity < <(acpi -b | awk -F'[,:%]' '{print $2, $3}')
 	    if [ "$status" = Discharging ]; then
             systemctl suspend
         fi
     elif [ "$status" = Discharging ] && [ "$capacity" -lt 15 ] && [ "$lowbatnotsent" != yes ]; then
 		lowbatnotsent=yes
-		mpv --no-config /usr/local/share/ding.mp3; notify-send 'Battery low!' 'Plug in your charger now.' &
+		mpv --no-config /usr/local/share/ding.mp3; notify-send --app-name='Battery low!' 'Plug in your charger now.' &
 	elif [ "$status" = Charging ] || [ "$capacity" -ge 15 ]; then
 		lowbatnotsent=no
 		critbatnotsent=no
