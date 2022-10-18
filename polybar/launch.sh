433fd75e8a6f5214acd475b254e5cca41b5a739d
Yottaphy <jorge13romero@gmail.com>
2022-10-18 18:03:09 +0100

    updated as of Oct 2022

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,13 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1;done
 
 #Launch bar1 and bar2
-polybar floatingleft &
-polybar floatingcentre &
-polybar floatingright &
-
 external=$(xrandr --query | grep 'HDMI-A-0')
 if [[ $external = *connected* ]]; then
     polybar floatingleft_ext &
     polybar floatingcentre_ext &
     polybar floatingright_ext &
 fi
+
+polybar floatingleft &
+polybar floatingcentre &
+polybar floatingright &
+
+
