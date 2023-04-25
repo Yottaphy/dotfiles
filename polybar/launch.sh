ad2f1b5c63db69f93a1c87350d1b7ebcee29c5bb
Yottaphy <jorge13romero@gmail.com>
2023-04-25 12:52:47 +0100

    fidgetted with bspwmrc and polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,24 +6,9 @@ killall -q polybar
 #Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1;done
 
-#Launch bar1 and bar2
-externalHDMI=$(xrandr --query | grep 'HDMI-A-0')
-if [[ $externalHDMI = *connected* ]]; then
-    polybar floatingleft_ext &
-    polybar floatingcentre_ext &
-    polybar floatingright_ext &
-fi
-
-externalDP=$(xrandr --query | grep 'DisplayPort-0')
-if [[ $externalDP = *connected* ]]; then
-    polybar floatingleft_ext2 &
-    polybar floatingcentre_ext2 &
-    polybar floatingright_ext2 &
-fi
-
-
-polybar floatingleft &
-polybar floatingcentre &
-polybar floatingright &
-
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload floatingleft &
+    MONITOR=$m polybar --reload floatingcentre &
+    MONITOR=$m polybar --reload floatingright &
+done
 
